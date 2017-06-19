1de852504ac3115b6aff07ea23f6948e4aef6e5c
Joshua Miller <jlmiller07@gmail.com>
2017-06-18 23:37:38 -0500

    added auto refresh

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,14 +11,19 @@
   firebase.initializeApp(config);
   var database = firebase.database();
 
+// GLOBAL VARIABLES
   var trainName = "";
   var destination = "";
   var firstTrainTime = "";
+  var firstTrainTimeArray = [];
   var frequency = "";
+  var frequencyArray = [];
   var nextArrival = "";
   var minutesAway = "";
   var html = "";
+  var counter = 0;
 
+// PUSHES DATA TO DATABASE ON SUBMIT
 $("#submit-button").on("click", function(event) {
     event.preventDefault();
 
@@ -26,7 +31,6 @@ $("#submit-button").on("click", function(event) {
     destination = $("#destination").val().trim();
     frequency = $("#frequency").val().trim();
     firstTrainTime = $("#first-train").val().trim();
-
 
     database.ref().push({
     trainName: trainName,
@@ -41,56 +45,63 @@ $("#submit-button").on("click", function(event) {
     $("#first-train").val('');
   });
 
-
+//UPDATES TABLE ON LOAD AND WHEN CHILD IS ADDED, UPDATES NEXT ARRIVAL AND MINUTES AWAY EVERY 10 SECONDS
 database.ref().orderByChild('employeeRole').on("child_added", function(snapshot) {
 
       var html = $('<div class="row">')
 
       firstTrainTime = snapshot.val().firstTrainTime;
+      firstTrainTimeArray.push(firstTrainTime);
+
       frequency = snapshot.val().frequency;
+      frequencyArray.push(frequency);
 
       nextTrainTime(firstTrainTime, frequency);
 
       html.append('<div class="column">' + (snapshot.val().trainName) + '</div>');
       html.append('<div class="column">' + (snapshot.val().destination) + '</div>');
       html.append('<div class="column">' + (snapshot.val().frequency) + '</div>');
-      html.append('<div class="column">' + moment(nextArrival).format("hh:mm a") + '</div>');
-      html.append('<div class="column last">' + minutesAway + '</div>');
-      $("#train-display").append(html);     
+      html.append('<div class="column" id="next-arrival-' + counter + '">' + moment(nextArrival).format("hh:mm a") + '</div>');
+      html.append('<div class="column last" id="minutes-away-' + counter + '">' + minutesAway + '</div>');
+      $("#train-display").append(html);
+      counter++;
+      setTimeout(update, 1000);
       });
 
+  update();
     
 
 
-  function nextTrainTime (firstTrainTime, frequency) {
-    
-    console.log(firstTrainTime);
-    console.log(frequency);
+  function nextTrainTime(firstTrainTime, frequency) {
 
     // First Time (pushed back 1 year to make sure it comes before current time)
     var firstTrainTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
-    console.log(firstTrainTimeConverted);
 
     // Current Time
     var currentTime = moment();
-    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
     // Difference between the times
     var diffTime = moment().diff(moment(firstTrainTimeConverted), "minutes");
-    console.log("DIFFERENCE IN TIME: " + diffTime);
 
     // Time apart (remainder) -- [minutes since last train]
     var tRemainder = diffTime % frequency;
-    console.log(tRemainder);
 
     // Minute Until Train
     minutesAway = frequency - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + minutesAway);
 
     // Next Train
     nextArrival = moment().add(minutesAway, "minutes");
-    console.log("ARRIVAL TIME: " + moment(nextArrival).format("hh:mm"));
   }
 
-  
+  function update() {
+    for (var i = 0; i < counter; i++) {
+      //console.log('update:' + firstTrainTimeArray[i]);
+      //console.log('update' + frequencyArray[i]);
+      nextTrainTime(firstTrainTimeArray[i], frequencyArray[i]);
+      $('#next-arrival-' + i).html(moment(nextArrival).format("hh:mm a"));
+      $('#minutes-away-' + i).html(minutesAway);
+      setTimeout(update, 1000);
+      
+        }    
+  }
 
